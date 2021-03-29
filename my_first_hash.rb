729ce855b699b7d03d805b7ddd6524393ab5b027
langan.kg Langan <langan.kg@gmail.com>
2021-03-29 14:24:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,7 +40,7 @@ end
 # To add a key to the person hash:
 person["hometown"] = "Massena, NY"
 person["hometown"] #=> "Massena, NY"
- 
+
 # Let's add another key
 person["favorite_thing"] = "Books"
 person["favorite_thing"] #=> "Books"
