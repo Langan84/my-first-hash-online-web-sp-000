fb073ac3e777597267a0d2cffe05c8a0dc1e85ce
langan.kg Langan <langan.kg@gmail.com>
2021-03-29 14:25:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,12 +25,6 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
-    "whale bone corsets" => 5,
-    "oil paintings" => 3,
-    "porcelain vases" => 2
-  }
-
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
 shipping_manifest
